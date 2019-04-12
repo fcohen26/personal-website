398c3a59079f2352092476eccac403ca0355d78f
Frances Cohen <fcohen26@gmail.com>
2019-04-11 20:03:42 -0400

    final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let seconds = 0;
+function incrementSeconds() {
+  seconds += 1;
+  $('#main').html(`You have been here for ${seconds} seconds!`);
+}
+setInterval(incrementSeconds, 1000);
